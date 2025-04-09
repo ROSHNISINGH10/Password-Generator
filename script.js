299b77f2f9b22616091393650cb89dc4f0e4e550
roshniSingh <149031647+ROSHNISINGH10@users.noreply.github.com>
2025-04-10 01:40:02 +0530

    Add files via upload
    Initial commit - password generator
    This project is a simple password generator built using HTML, CSS, and JavaScript.

---
+++ script.js
@@ -0,0 +1,21 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+<>?,./[]':"
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    document.getElementById("result").textContent = password;
+    
+  }
+  
+  function copyPassword() {
+    const password = document.getElementById("result").textContent;
+    navigator.clipboard.writeText(password).then(() => {
+      alert("Password copied!");
+
+    });
+
+  }
